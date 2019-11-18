5488818162afcbd3ee2991ee62ce1d03741c911d
Elvis Ibarra <ielvisd@gmail.com>
2019-11-17 20:34:17 -0800

    Backend & Frontend are communicating

--- jiujitsio-frontend/layouts/default.vue
+++ jiujitsio-frontend/layouts/default.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>
-    <nuxt/>
+  <div id="app">
+    <main class="app-main">
+      <nuxt />
+    </main>
   </div>
 </template>
 
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -16,7 +19,9 @@ html {
   box-sizing: border-box;
 }
 
-*, *:before, *:after {
+*,
+*:before,
+*:after {
   box-sizing: border-box;
   margin: 0;
 }
--- jiujitsio-frontend/pages/index.vue
+++ jiujitsio-frontend/pages/index.vue
@@ -1,35 +1,28 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        jiujitsio-frontend
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <h1>Jiujitsio</h1>
+    <div v-for="example in examples" :key="example.id">
+      <p>{{example.name}}</p>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
+  data() {
+    return {
+      examples: []
+    };
+  },
+  methods: {
+    async updateExamples() {
+      this.examples = await this.$axios.$get("/examples");
+    }
+  },
+  mounted() {
+    this.updateExamples();
   }
-}
+};
 </script>
 
 <style>
@@ -42,7 +35,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
